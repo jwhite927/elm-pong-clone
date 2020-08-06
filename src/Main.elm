20f75d20037d8ebd5d273713af9a62c191a88109
JoshWhite <jwhite927@gmail.com>
2020-08-05 21:27:40 -0400

    feat: Improve Game Logic

--- src/Main.elm
+++ src/Main.elm
@@ -1,48 +1,105 @@
--- Walk around with the arrow keys. Press the UP arrow to jump!
--- Learn more about the playground here:
--- https://package.elm-lang.org/packages/evancz/elm-playground/latest/
-
 module Main exposing (..)
 
 import Playground exposing (..)
 
-
 -- UPDATE
 
-update computer player =
+update : Computer -> State -> State
+update computer memory =
+
     let
         vy = 10 * toY computer.keyboard
-        x = player.x
-        y = player.y + vy
+        y = memory.p1.y + vy
+        bvx = if hittingPaddle memory (whichSide memory) then
+                case whichSide memory of
+                    Left -> 3
+                    Right -> -3
+              else memory.bvx
+        bvy = if hittingTorB computer memory.by then negate memory.bvy else memory.bvy
+        p1 = memory.p1
     in
-        { x = x
-        , y = max 0 y
-        , vy = vy
+        { memory
+        | p1 = { p1  | y = (max computer.screen.bottom y), vy = vy }
+        , bx = memory.bx + bvx
+        , by = memory.by + bvy
+        , bvx = bvx
+        , bvy = bvy
         }
 
 
+hittingTorB computer ballY =
+    ballY + 10 > computer.screen.top
+    || ballY - 10 < computer.screen.bottom
+
+timeToCheck memory side =
+    case side of
+        Left  ->
+            memory.bx < memory.p1.x + 20
+        Right ->
+            memory.bx > memory.p2.x - 20
+
+type Side
+    = Left
+    | Right
+
+whichSide memory =
+    if memory.bx < 0 then Left else Right
+
+hittingPaddle memory side =
+    timeToCheck memory side
+    && case side of
+       Left ->
+           memory.by - 10 < memory.p1.y + 45
+           && memory.by + 10 > memory.p1.y - 45
+       Right ->
+           memory.by - 10 < memory.p2.y + 45
+           && memory.by + 10 > memory.p2.y - 45
+
 -- VIEW
 
-view computer player =
+view: Computer -> State -> List Shape
+view computer memory =
     let
         w = computer.screen.width
         h = computer.screen.height
-        b = computer.screen.bottom
     in
-        [ rectangle (rgb 174 238 238) w h
-        , rectangle (rgb 50 50 50) 20 90
-            |> move player.x (b + 76 + player.y)
-        , rectangle (rgb 50 50 50) 20 90
-            |> move (player.x + 600) (b + 76 + player.y)
-        ]
-
+        [ rectangle (rgb 0 0 0) w h
+        , rectangle (rgb 250 250 250) 20 90
+            |> move memory.p1.x memory.p1.y
+        , rectangle (rgb 250 250 250) 20 90
+            |> move memory.p2.x memory.p2.y
+        , square (rgb 250 250 250) 20
+            |> move memory.bx memory.by
+         ]
 
 -- MAIN
 
-main =
-    game view update
-    { x = -300
-    , y = 0
-    , vy = 0
+type alias Paddle =
+    { x: Float
+    , y : Float
+    , vy: Float
     }
+
+initLeftPaddle =
+    Paddle -300 0 0
+
+initRightPaddle =
+    Paddle 300 0 0
+
+type alias State =
+    { p1 : Paddle
+    , p2 : Paddle
+    , bx : Float
+    , by : Float
+    , bvx : Float
+    , bvy : Float
+    }
+
+initState : State
+initState =
+    State initLeftPaddle initRightPaddle 0 0 -3 0
+
+main =
+    game view update initState
+
 
